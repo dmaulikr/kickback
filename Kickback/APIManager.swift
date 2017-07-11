f4ff9f9e0add240b1c069bb9ec83c42ba6906dcc
Daniel Afolabi <dafolabi@fb.com>
2017-07-11 14:07:09 -0700

    login button connected to spotify

--- Kickback/APIManager.swift
+++ Kickback/APIManager.swift
@@ -11,24 +11,54 @@ import Foundation
 class APIManager {
     var auth = SPTAuth.defaultInstance()!
     
-//    var session:SPTSession!
-//    var player: SPTAudioStreamingController?
+    var session:SPTSession!
+    var player: SPTAudioStreamingController?
     
-    var loginUrl: URL?
+    var loginURL: URL?
     
     func setup() {
-      
-        SPTAuth.defaultInstance().clientID = "7d5032c6d7294aeb8a4fdc7662062655"
-        SPTAuth.defaultInstance().redirectURL = URL(string: "Kickback://returnAfterLogin")
-        SPTAuth.defaultInstance().requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
-        loginUrl = SPTAuth.defaultInstance().spotifyWebAuthenticationURL()
+        let clientID = "7d5032c6d7294aeb8a4fdc7662062655" // put your client ID here
+        let redirectURL = "Kickback://returnAfterLogin" // put your direct URL here
+        auth.clientID = clientID
+        auth.redirectURL = URL(string: redirectURL)
+        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
+        loginURL = auth.spotifyWebAuthenticationURL()
+        print("Setup login URL: " + String(describing: loginURL))
     }
     
     func login() {
-        if UIApplication.shared.openURL(loginUrl!) {
-            if auth.canHandle(auth.redirectURL) {
-                // To do - build in error handling
-            }
+        print("Login URL: " + String(describing: loginURL))
+        if UIApplication.shared.openURL(loginURL!) {
+//            if let error = error {
+//                
+//            }
+//            if auth.canHandle(auth.redirectURL) {
+//                success()
+//            } else {
+//                
+//                failure(error)
+//            }
         }
     }
+        
+    func initializePlayer(authSession:SPTSession){
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.sharedInstance()
+            self.player!.playbackDelegate = self as! SPTAudioStreamingPlaybackDelegate
+            self.player!.delegate = self as! SPTAudioStreamingDelegate
+            try! player!.start(withClientId: auth.clientID)
+            self.player!.login(withAccessToken: authSession.accessToken)
+        }
+    }
+    
+//    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+//        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
+//        print("logged in")
+//        self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+//            if (error != nil) {
+//                print("playing!")
+//            }
+//        })
+//    }
+    
 }
--- Kickback/LoginViewController.swift
+++ Kickback/LoginViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    var auth = SPTAuth.defaultInstance()!
-
-    var session:SPTSession!
-    var player: SPTAudioStreamingController?
+    
+    let manager = APIManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        APIManager().setup()
-        NotificationCenter.default.addObserver(self, selector: #selector(updateAfterFirstLogin))
+        manager.setup()
+        NotificationCenter.default.addObserver(self, selector: #selector(updateAfterFirstLogin), name: nil, object: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,39 +25,26 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func updateAfterFirstLogin () {
+    func updateAfterFirstLogin() {
         let userDefaults = UserDefaults.standard
         if let sessionObject = userDefaults.object(forKey: "SpotifySession") {
             let sessionsDataObj = sessionObject as! Data
-            let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionsDataObj) as! SPTSesssion
-            self.session = firstTimeSession
-            initializePlayer(authSession: session)
+            let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionsDataObj) as! SPTSession
+            manager.session = firstTimeSession
+            manager.initializePlayer(authSession: manager.session)
         }
-    }
-    
-    func initializePlayer(authSession:SPTSession){
-        if self.player == nil {
-            self.player = SPTAudioStreamingController.sharedInstance()
-            self.player!.playbackDelegate = self as! SPTAudioStreamingPlaybackDelegate
-            self.player!.delegate = self as! SPTAudioStreamingDelegate
-            try! player!.start(withClientId: auth.clientID)
-            self.player!.login(withAccessToken: authSession.accessToken)
-        }
-    }
-    
-    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
-        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
-        print("logged in")
-        self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
-            if (error != nil) {
-                print("playing!")
-            }
-        })
     }
     
     @IBAction func didTapLogin(_ sender: Any) {
-        APIManager().login()
-        performSegue(withIdentifier: "toHomeViewController", sender: self)
+        manager.login()
+            performSegue(withIdentifier: "toHomeViewController", sender: nil)
+        
+//        
+//        if let error = error {
+//            print("Error: \(error.localizedDescription)")
+//        } else {
+//            performSegue(withIdentifier: "toHomeViewController", sender: self)
+//        }
     }
     
     /*
