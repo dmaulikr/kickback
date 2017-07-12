e7b9015ade74c7e5c5a7ea48c8688d4c311be412
Katie Jiang <katiejiang@fb.com>
2017-07-12 10:07:09 -0700

    Queue add/remove user, track methods linked with Parse, check if access code exists

--- Kickback/Queue.swift
+++ Kickback/Queue.swift
@@ -17,7 +17,7 @@ class Queue {
     var accessCode: String
     var tracks: [Track]
     var counts: [String: Int] // user id : number of songs user has played
-    var members: Set<String> // user ids, might not be necessary with the counts dictionary?
+    var members: [String] // user ids, might not be necessary with the counts dictionary?
     var playIndex: Int // index of current playing track
     var currentTrack: Track?
     var parseQueue: PFObject
@@ -30,8 +30,7 @@ class Queue {
         self.accessCode = Queue.generateAccessCode()
         self.tracks = []
         self.counts = [owner.id: 0]
-        self.members = Set<String>()
-        self.members.insert(owner.id)
+        self.members = [owner.id]
         self.playIndex = -1
         self.currentTrack = nil
         queue["tracks"] = self.tracks
@@ -48,7 +47,7 @@ class Queue {
             try parseQueue.fetch()
             self.tracks = parseQueue["tracks"] as! [Track]
             self.counts = parseQueue["counts"] as! [String: Int]
-            self.members = parseQueue["members"] as! Set<String>
+            self.members = parseQueue["members"] as! [String]
             self.playIndex = parseQueue["playIndex"] as! Int
             self.currentTrack = parseQueue["currentTrack"] as? Track
         } catch {
@@ -58,35 +57,53 @@ class Queue {
     
     func addMember(userId: String) {
         if !members.contains(userId) {
-            members.insert(userId)
+            updateFromParse()
+            members.append(userId)
+            parseQueue.add(userId, forKey: "members")
             counts[userId] = 0
+            parseQueue["counts"] = counts
+            parseQueue.saveInBackground()
         }
     }
     
     func removeMember(userId: String) {
         if members.contains(userId) {
-            members.remove(userId)
+            updateFromParse()
+            members = members.filter() {$0 != userId}
+            parseQueue.remove(userId, forKey: "members")
             counts.removeValue(forKey: userId)
+            parseQueue["counts"] = counts
+            parseQueue.saveInBackground()
         }
     }
     
-    func addTrack(_ track: Track) {
+    func addTrack(_ track: Track, user: User) {
+        track.queuedBy = user
+        updateFromParse()
         tracks.append(track)
+        parseQueue.add(track, forKey: "tracks")
         // reorder the tracks as needed (we might need to use a heap)
         counts[track.queuedBy!.id]! += 1
+        parseQueue["counts"] = counts
+        parseQueue.saveInBackground()
     }
     
     private static func generateAccessCode() -> String {
-        var code = ""
         let possible : NSString = "abcdefghijklmnopqrstuvwxyz"
-        // TODO: check if access code already exists
-        for _ in 1...6 {
-            let random = arc4random_uniform(UInt32(possible.length))
-            var char = possible.character(at: Int(random))
-            code += NSString(characters: &char, length: 1) as String
+        var codeExists = true
+        var code: String = ""
+        while codeExists {
+            code = ""
+            for _ in 1...6 {
+                let random = arc4random_uniform(UInt32(possible.length))
+                var char = possible.character(at: Int(random))
+                code += NSString(characters: &char, length: 1) as String
+            }
+            let query = PFQuery(className: "Queue").whereKey("accessCode", equalTo: code)
+            if query.countObjects(nil) == 0 {
+                codeExists = false
+            }
         }
         return code
     }
-    
-    
 }
